20a6660a2a55fd4d5c2d215b76ec829e8b37e5a4
dpvirk3 <dpvirk3@gmail.com>
2021-06-10 22:27:52 +0530

    make add remove items in cart functional

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,27 +1,50 @@
 import styles from "./Cart.module.css";
 import Modal from "../UI/Modal";
-
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmt = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const addItemHandler = (item) => {
+    cartCtx.addItem({id: item.id, amount: 1})
+  }
+
+  const removeItemHandler = (id) => {
+    cartCtx.removeItem(id);
+  }
+
   //example cart item
   const cartItems = (
-    <ul className={styles["cart-item"]}>
-      {[{ id: "C1", name: "Sushi", amount: 2, price: 12.99 },].map((item) => {
-       return <li>{item.name}</li>;
+    <ul className={styles['cart-item']}>
+      {cartCtx.items.map((item) => {
+        return <CartItem 
+        key= {item.id}
+        name={item.name} 
+        amount={item.amount}
+        price={item.price}
+        onAdd={addItemHandler.bind(null, item)}
+        onRemove={removeItemHandler.bind(null, item.id)} />;
       })}
     </ul>
   );
 
   return (
-    <Modal onBackdropClick={props.onHide} >
+    <Modal onBackdropClick={props.onHide}>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmt}</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles["button--alt"]} onClick={props.onHide}>close</button>
-        <button className={styles.button}>Order</button>
+        <button className={styles["button--alt"]} onClick={props.onHide}>
+          close
+        </button>
+        {hasItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/store/CartCtxProvider.js
+++ src/store/CartCtxProvider.js
@@ -9,15 +9,61 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
 
   if (action.type === 'ADD_ITEM') {
-    const updatedItems = state.items.concat(action.item);
+   // const updatedItems = state.items.concat(action.item);
     const updatedAmt = state.totalAmt + action.item.price * action.item.amount;
+   
+   //check if the added item exists in the list
+   const addedItemIndex = state.items.findIndex(
+     item =>  item.id === action.item.id
+   );
+
+   const existingItem = state.items[addedItemIndex];
+
+   let updatedItemArray;
+   if (existingItem) {
+    const updatedItem = {...existingItem, 
+      amount: existingItem.amount + action.item.amount};
+      updatedItemArray = [...state.items];
+      updatedItemArray[addedItemIndex]=updatedItem;
+   }
+   else {
+     updatedItemArray = state.items.concat(action.item);
+   }
+   
     return {
-      items: updatedItems,
+      items: updatedItemArray,
       totalAmt: updatedAmt
     }
   }
   else if (action.type === 'REMOVE_ITEM') {
-    console.log ('remove item not yet implemented');
+    const removedItemIndex = state.items.findIndex(
+      item=> item.id === action.id
+    );
+
+
+    const removedItem = state.items[removedItemIndex];
+    const remainingCount = removedItem.amount - 1;
+    const updatedAmt = state.totalAmt - removedItem.price;
+    let updatedItemArray;
+
+    if (removedItem && remainingCount > 0) {
+      const updatedItem = {...removedItem, 
+        amount: removedItem.amount - 1};
+      updatedItemArray = [...state.items];
+      updatedItemArray[removedItemIndex] = updatedItem;
+    }
+    else if (removedItem && remainingCount ===0) {
+      // updatedItemArray = [...state.items];
+      // updatedItemArray.splice(removedItemIndex, 1);
+      //alternately use filer function on array
+      updatedItemArray = state.items.filter (item =>
+        item.id !== action.id);
+    }
+    return {
+      items: updatedItemArray,
+      totalAmt: updatedAmt
+    }
+    //console.log ('remove item not yet implemented');
   }
 
   return defaultCartState
