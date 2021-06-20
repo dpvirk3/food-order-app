5d1d159429ab8e51b0db0eeae32b43bb509bce11
dpvirk3 <dpvirk3@gmail.com>
2021-06-20 15:45:45 +0530

    fix styling

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,35 +3,56 @@ import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
 import Checkout from "./Checkout";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  const [doCheckout, setDoCheckout] = useState(false);
 
   const totalAmt = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
   const addItemHandler = (item) => {
-    cartCtx.addItem({id: item.id, amount: 1})
-  }
+    cartCtx.addItem({ id: item.id, amount: 1 });
+  };
 
   const removeItemHandler = (id) => {
     cartCtx.removeItem(id);
-  }
+  };
+
+  const onOrderHandler = (event) => {
+    setDoCheckout(true);
+  };
 
   //example cart item
   const cartItems = (
-    <ul className={styles['cart-items']}>
+    <ul className={styles["cart-items"]}>
       {cartCtx.items.map((item) => {
-        return <CartItem 
-        key= {item.id}
-        name={item.name} 
-        amount={item.amount}
-        price={item.price}
-        onAdd={addItemHandler.bind(null, item)}
-        onRemove={removeItemHandler.bind(null, item.id)} />;
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onAdd={addItemHandler.bind(null, item)}
+            onRemove={removeItemHandler.bind(null, item.id)}
+          />
+        );
       })}
     </ul>
+  );
+
+  const cartActions = (
+    <div className={styles.actions}>
+      <button className={styles["button--alt"]} onClick={props.onHide}>
+        close
+      </button>
+      {hasItems && (
+        <button className={styles.button} onClick={onOrderHandler}>
+          Order
+        </button>
+      )}
+    </div>
   );
 
   return (
@@ -40,15 +61,9 @@ const Cart = (props) => {
       <div className={styles.total}>
         <span>Total Amount</span>
         <span>{totalAmt}</span>
-    
       </div>
-      <Checkout />
-      <div className={styles.actions}>
-        <button className={styles["button--alt"]} onClick={props.onHide}>
-          close
-        </button>
-        {hasItems && <button className={styles.button}>Order</button>}
-      </div>
+      {doCheckout && <Checkout onCancel={props.onHide} />}
+      {!doCheckout && cartActions}
     </Modal>
   );
 };
--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -1,8 +1,15 @@
 import styles from "./Checkout.module.css";
 
 const Checkout = (props) => {
+  const submitHandler = (event) => {
+      event.preventDefault();
+  };
+
+
+  //the cancel button gets the onClick which is props 
+  //to pass the action up to close the modal. - this could be context
   return (
-    <form>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles.control}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" />
@@ -19,8 +26,11 @@ const Checkout = (props) => {
         <label htmlFor="postalCode">Postal Code</label>
         <input type="text" id="postalCode" />
       </div>
-      <div className={styles.control}>
-        <button>Confirm</button>
+      <div className={styles.actions}>
+        <button type="button" className={styles['button--alt']} onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={styles.submit}>Confirm</button>
       </div>
     </form>
   );
